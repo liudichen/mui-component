643b97f30d9289095c63986d09c80ffc2cd48830
iimm <liudichen@foxmail.com>
2022-07-13 16:12:47 +0800

    仅当是第1页时可以自动隐藏页脚

--- src/components/SimpleTable/index.jsx
+++ src/components/SimpleTable/index.jsx
@@ -8,7 +8,15 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { useControllableValue, useCreation } from 'ahooks';
-import { Box, Table, TableBody, TableFooter, TableHead, TablePagination, TableRow } from '@mui/material';
+import {
+  Box,
+  Table,
+  TableBody,
+  TableFooter,
+  TableHead,
+  TablePagination,
+  TableRow,
+} from '@mui/material';
 
 import { columnPropType } from './common';
 import Row from './Row';
@@ -16,21 +24,57 @@ import Row from './Row';
 const SimpleTable = (props) => {
   const {
     tableContainerBoxProps,
-    columns, rows, rowKey,
+    columns,
+    rows,
+    rowKey,
     // eslint-disable-next-line no-unused-vars
-    total, current, pageSize, pageSizeOptions, onPageChange, onPageSizeChange, paginationProps, initPageSize,
-    hideHeader, title, titlePosition, titleStyle, bordered, hideFooter, columnDefaultWidth,
-    expandable, showExpandColumn, expandIcon, expandRowByClick, expandColumnWidth, expandRowRender, getRowExpandable, unmountOnExit,
+    total,
+    current,
+    pageSize,
+    pageSizeOptions,
+    onPageChange,
+    onPageSizeChange,
+    paginationProps,
+    initPageSize,
+    hideHeader,
+    title,
+    titlePosition,
+    titleStyle,
+    bordered,
+    hideFooter,
+    columnDefaultWidth,
+    expandable,
+    showExpandColumn,
+    expandIcon,
+    expandRowByClick,
+    expandColumnWidth,
+    expandRowRender,
+    getRowExpandable,
+    unmountOnExit,
     ...restProps
   } = props;
   const colSpans = useCreation(() => {
     let cols = columns?.length ?? 0;
-    if (showExpandColumn) { cols += 1; }
+    if (showExpandColumn) {
+      cols += 1;
+    }
     return cols;
-  }, [ columns?.length, showExpandColumn ]);
-  const [ pageNumber, setPageNumber ] = useControllableValue(props, { valuePropName: 'current', trigger: 'onPageChange', defaultValue: 1 });
-  const [ rowsPerPage, setRowsPerPage ] = useControllableValue(props, { defaultValuePropName: 'initPageSize', valuePropName: 'pageSize', trigger: 'onPageSizeChange', defaultValue: 25 });
-  const dataSource = (typeof total !== 'undefined' || !rowsPerPage) ? rows : rows?.slice(rowsPerPage * (pageNumber - 1), rowsPerPage * pageNumber);
+  }, [columns?.length, showExpandColumn]);
+  const [pageNumber, setPageNumber] = useControllableValue(props, {
+    valuePropName: 'current',
+    trigger: 'onPageChange',
+    defaultValue: 1,
+  });
+  const [rowsPerPage, setRowsPerPage] = useControllableValue(props, {
+    defaultValuePropName: 'initPageSize',
+    valuePropName: 'pageSize',
+    trigger: 'onPageSizeChange',
+    defaultValue: 25,
+  });
+  const dataSource =
+    typeof total !== 'undefined' || !rowsPerPage
+      ? rows
+      : rows?.slice(rowsPerPage * (pageNumber - 1), rowsPerPage * pageNumber);
   return (
     <Box
       {...(tableContainerBoxProps || {})}
@@ -44,11 +88,13 @@ const SimpleTable = (props) => {
       <Table {...restProps}>
         {!!title && (
           <caption
-            style={{ ...{
-              padding: 0,
-              captionSide: titlePosition,
-              ...(titleStyle || {}),
-            } }}
+            style={{
+              ...{
+                padding: 0,
+                captionSide: titlePosition,
+                ...(titleStyle || {}),
+              },
+            }}
           >
             {title}
           </caption>
@@ -67,7 +113,7 @@ const SimpleTable = (props) => {
           </TableHead>
         )}
         <TableBody>
-          { dataSource?.map((item, index) => {
+          {dataSource?.map((item, index) => {
             let isExpandable = true;
             if (typeof getRowExpandable === 'function') {
               isExpandable = getRowExpandable(item, index) ?? true;
@@ -91,14 +137,14 @@ const SimpleTable = (props) => {
                 columnDefaultWidth={columnDefaultWidth}
               />
             );
-          })
-          }
+          })}
         </TableBody>
-        {(hideFooter === false || (hideFooter === undefined && (rows?.length || 0) > initPageSize)) && (
+        {(hideFooter === false ||
+          (hideFooter === undefined && pageNumber === 1 && (rows?.length || 0) > initPageSize)) && (
           <TableFooter>
             <TableRow>
               <TablePagination
-                labelRowsPerPage='每页行数:'
+                labelRowsPerPage="每页行数:"
                 showFirstButton
                 showLastButton
                 {...(paginationProps || {})}
@@ -121,7 +167,7 @@ const SimpleTable = (props) => {
 SimpleTable.defaultProps = {
   titlePosition: 'top',
   initPageSize: 10,
-  pageSizeOptions: [ 10, 25, 50, 100, 200 ],
+  pageSizeOptions: [10, 25, 50, 100, 200],
   showExpandColumn: true,
   expandColumnWidth: 45,
   columnDefaultWidth: 100,
@@ -130,13 +176,10 @@ SimpleTable.defaultProps = {
 SimpleTable.propTypes = {
   component: PropTypes.elementType,
   classes: PropTypes.object,
-  padding: PropTypes.oneOf([ 'normal', 'checkbox', 'none' ]),
-  size: PropTypes.oneOfType([
-    PropTypes.oneOf([ 'medium', 'small' ]),
-    PropTypes.string,
-  ]),
+  padding: PropTypes.oneOf(['normal', 'checkbox', 'none']),
+  size: PropTypes.oneOfType([PropTypes.oneOf(['medium', 'small']), PropTypes.string]),
   stickyHeader: PropTypes.bool,
-  sx: PropTypes.oneOfType([ PropTypes.array, PropTypes.func, PropTypes.object ]),
+  sx: PropTypes.oneOfType([PropTypes.array, PropTypes.func, PropTypes.object]),
 
   tableContainerBoxProps: PropTypes.object,
   rows: PropTypes.array,
@@ -144,16 +187,16 @@ SimpleTable.propTypes = {
   columns: PropTypes.arrayOf(PropTypes.shape(columnPropType)),
   hideHeader: PropTypes.bool,
   title: PropTypes.node,
-  titlePosition: PropTypes.oneOf([ 'top', 'bottom', 'inherit' ]),
+  titlePosition: PropTypes.oneOf(['top', 'bottom', 'inherit']),
 
   bordered: PropTypes.bool,
-  columnDefaultWidth: PropTypes.oneOfType([ PropTypes.number, PropTypes.string ]),
+  columnDefaultWidth: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
 
   expandable: PropTypes.bool,
   showExpandColumn: PropTypes.bool,
   expandIcon: PropTypes.array,
   expandRowByClick: PropTypes.bool,
-  expandColumnWidth: PropTypes.oneOfType([ PropTypes.number, PropTypes.string ]),
+  expandColumnWidth: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
   expandRowRender: PropTypes.func, // (row,index,open)=>ReactNode
   getRowExpandable: PropTypes.func, // (row,index) => boolean
   unmountOnExit: PropTypes.bool,
