ba7c40ac9102162ae8afe984959d9d9fbf3ef892
iimm <liudichen@foxmail.com>
2022-11-22 10:18:50 +0800

    ->0.1.45 依赖整理

--- src/components/DataGridTable/NoRowsOverlay/index.jsx
+++ src/components/DataGridTable/NoRowsOverlay/index.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Box } from '@mui/material';
-import { styled } from '@mui/styles';
-
+import { Box, styled } from '@mui/material';
 
 const StyledGridOverlay = styled('div')(({ theme }) => ({
   display: 'flex',
