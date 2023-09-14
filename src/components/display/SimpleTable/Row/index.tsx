466db08d6f9cbbfb860c94d7ae5479c5f90375da
iimm <liudichen@foxmail.com>
2023-09-14 15:06:15 +0800

    修正@tabler/icons-react的IconSquarePlus图标变化导致的显示不符合预期

--- src/components/display/SimpleTable/Row/index.tsx
+++ src/components/display/SimpleTable/Row/index.tsx
@@ -1,47 +1,81 @@
-import { useCreation, useSafeState } from 'ahooks';
-import { Collapse, TableCell, TableRow } from '@mui/material';
-import { IconSquarePlus as OpenIcon, IconSquareMinus as CloseIcon } from '@tabler/icons-react';
+import { useCreation, useSafeState } from "ahooks";
+import { Collapse, TableCell, TableRow } from "@mui/material";
+import { createReactComponent, IconSquareMinus as CloseIcon } from "@tabler/icons-react";
 
-import { Cell } from './Cell';
-import type { SimpleTableColumn } from '../index';
+const OpenIcon = createReactComponent("square-plus", "IconSquarePlus", [
+  [
+    "path",
+    {
+      d: "M3 3m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z",
+      key: "svg-0",
+    },
+  ],
+  ["path", { d: "M9 12l6 0", key: "svg-1" }],
+  ["path", { d: "M12 9l0 6", key: "svg-2" }],
+]);
+
+import { Cell } from "./Cell";
+import type { SimpleTableColumn } from "../index";
 
 export const Row = (props: any) => {
-  const { columns, rowIndex, hideHeader, row, showExpandColumn, expandRowByClick, expandColumnWidth, expandRowRender, expandable, isExpandable, expandIcon, bordered, unmountOnExit, columnDefaultWidth } = props;
-  const [ open, setOpen ] = useSafeState(false);
+  const {
+    columns,
+    rowIndex,
+    hideHeader,
+    row,
+    showExpandColumn,
+    expandRowByClick,
+    expandColumnWidth,
+    expandRowRender,
+    expandable,
+    isExpandable,
+    expandIcon,
+    bordered,
+    unmountOnExit,
+    columnDefaultWidth,
+  } = props;
+  const [open, setOpen] = useSafeState(false);
   const colSpans = useCreation(() => {
     let cols = columns?.length ?? 0;
-    if (showExpandColumn) { cols += 1; }
+    if (showExpandColumn) {
+      cols += 1;
+    }
     return cols;
-  }, [ columns?.length, showExpandColumn ]);
+  }, [columns?.length, showExpandColumn]);
   return (
     <>
       <TableRow>
         {expandable && showExpandColumn && (
           <TableCell
-            align='center'
-            width={(rowIndex === -1 || (rowIndex === 0 && hideHeader)) ? expandColumnWidth : undefined}
-            sx={bordered ? ((rowIndex === -1 || (rowIndex === 0 && hideHeader)) ? {
-              borderTop: '1px solid rgba(224,224,224,1)',
-              borderLeft: '1px solid rgba(224,224,224,1)',
-              borderRight: '1px solid rgba(224,224,224,1)',
-              maxWidth: expandColumnWidth,
-            } : {
-              borderLeft: '1px solid rgba(224,224,224,1)',
-              borderRight: '1px solid rgba(224,224,224,1)',
-              maxWidth: expandColumnWidth,
-            }) : undefined}
+            align="center"
+            width={rowIndex === -1 || (rowIndex === 0 && hideHeader) ? expandColumnWidth : undefined}
+            sx={
+              bordered
+                ? rowIndex === -1 || (rowIndex === 0 && hideHeader)
+                  ? {
+                      borderTop: "1px solid rgba(224,224,224,1)",
+                      borderLeft: "1px solid rgba(224,224,224,1)",
+                      borderRight: "1px solid rgba(224,224,224,1)",
+                      maxWidth: expandColumnWidth,
+                    }
+                  : {
+                      borderLeft: "1px solid rgba(224,224,224,1)",
+                      borderRight: "1px solid rgba(224,224,224,1)",
+                      maxWidth: expandColumnWidth,
+                    }
+                : undefined
+            }
           >
             {rowIndex > -1 && !!isExpandable && (
-              <span
-                onClick={() => setOpen((s) => !s)}
-                style={{ cursor: 'pointer' }}
-              >
-                { open ? (expandIcon?.[0] ?? <CloseIcon size='1.25rem'/>) : (expandIcon?.[1] ?? <OpenIcon size='1.25rem' />)}
+              <span onClick={() => setOpen((s) => !s)} style={{ cursor: "pointer" }}>
+                {open
+                  ? expandIcon?.[0] ?? <CloseIcon size="1.25rem" />
+                  : expandIcon?.[1] ?? <OpenIcon size="1.25rem" />}
               </span>
             )}
           </TableCell>
         )}
-        { columns?.map((item: SimpleTableColumn, index: number) => (
+        {columns?.map((item: SimpleTableColumn, index: number) => (
           <Cell
             key={index}
             row={row}
@@ -54,25 +88,24 @@ export const Row = (props: any) => {
             columnDefaultWidth={columnDefaultWidth}
             {...item}
           />
-        ))
-        }
+        ))}
       </TableRow>
       {isExpandable && rowIndex > -1 && open && (
         <TableRow>
           <TableCell
             colSpan={colSpans}
             sx={{ py: 0 }}
-            style={bordered ? {
-              borderLeft: '1px solid rgba(224,224,224,1)',
-              borderRight: '1px solid rgba(224,224,224,1)',
-            } : undefined}
+            style={
+              bordered
+                ? {
+                    borderLeft: "1px solid rgba(224,224,224,1)",
+                    borderRight: "1px solid rgba(224,224,224,1)",
+                  }
+                : undefined
+            }
           >
-            <Collapse
-              in={open}
-              timeout='auto'
-              unmountOnExit={unmountOnExit}
-            >
-              { expandRowRender?.(row, rowIndex, open)}
+            <Collapse in={open} timeout="auto" unmountOnExit={unmountOnExit}>
+              {expandRowRender?.(row, rowIndex, open)}
             </Collapse>
           </TableCell>
         </TableRow>
@@ -81,4 +114,4 @@ export const Row = (props: any) => {
   );
 };
 
-Row.displayName = 'iimm.Mui.SimpleTable.Row';
+Row.displayName = "iimm.Mui.SimpleTable.Row";
