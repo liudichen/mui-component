23c67ee32d61b3dbcf22091e3117f3da2667a8f1
liudichen <liudichen@foxmail.com>
2024-04-13 15:53:55 +0800

    1.16.2

--- src/components/feedback/Popover/index.tsx
+++ src/components/feedback/Popover/index.tsx
@@ -1,29 +1,22 @@
-import { type ReactNode, type PropsWithChildren, forwardRef } from 'react';
-import { useMemoizedFn, useSafeState } from 'ahooks';
-import { Popover as MuiPopover } from '@mui/material';
-import type { PopoverProps as MuiPopoverProps } from '@mui/material';
-import { useId } from '@iimm/react-shared';
+import { type ReactNode, type PropsWithChildren, forwardRef } from "react";
+import { useMemoizedFn, useSafeState } from "ahooks";
+import { Popover as MuiPopover } from "@mui/material";
+import type { PopoverProps as MuiPopoverProps } from "@mui/material";
+import { useId } from "@iimm/react-shared";
 
-export interface PopoverProps extends Omit<MuiPopoverProps, 'onClose' | 'content' | 'open'> {
+export interface PopoverProps extends Omit<MuiPopoverProps, "onClose" | "content" | "open"> {
   /** 触发弹出的操作,鼠标悬停或点击 */
-  triggerType?: 'hover' | 'click',
-  disabled?: boolean,
-  trigger: ReactNode,
-  content?: ReactNode,
-  onClose?: () => void,
-  open?: boolean
+  triggerType?: "hover" | "click";
+  disabled?: boolean;
+  trigger: ReactNode;
+  content?: ReactNode;
+  onClose?: () => void;
+  open?: boolean;
 }
 
-
 export const Popover = forwardRef<any, PropsWithChildren<PopoverProps>>((props, ref) => {
-  const {
-    onClose: onCloseProp,
-    triggerType, disabled,
-    trigger, id: idProp,
-    content, children,
-    ...restProps
-  } = props;
-  const [ anchorE1, setAnchorE1 ] = useSafeState(null);
+  const { onClose: onCloseProp, triggerType, disabled, trigger, id: idProp, content, children, ...restProps } = props;
+  const [anchorE1, setAnchorE1] = useSafeState(null);
   const id = useId(idProp);
   const handleColse = useMemoizedFn(() => {
     if (anchorE1 && !disabled) {
@@ -32,7 +25,9 @@ export const Popover = forwardRef<any, PropsWithChildren<PopoverProps>>((props, 
     }
   });
   const handleOpen = useMemoizedFn((e) => {
-    if (!disabled) { setAnchorE1(e.currentTarget); }
+    if (!disabled) {
+      setAnchorE1(e.currentTarget);
+    }
   });
   const open = Boolean(anchorE1);
   return (
@@ -40,10 +35,10 @@ export const Popover = forwardRef<any, PropsWithChildren<PopoverProps>>((props, 
       <span
         aria-owns={open ? id : undefined}
         aria-haspopup="true"
-        onMouseEnter={triggerType === 'hover' ? handleOpen : undefined }
-        onMouseLeave={triggerType === 'hover' ? handleColse : undefined}
-        onClick={triggerType === 'click' ? handleOpen : undefined}
-        style={{ cursor: 'help' }}
+        onMouseEnter={triggerType === "hover" ? handleOpen : undefined}
+        onMouseLeave={triggerType === "hover" ? handleColse : undefined}
+        onClick={triggerType === "click" ? handleOpen : undefined}
+        style={{ cursor: "help" }}
       >
         {trigger}
       </span>
@@ -54,11 +49,11 @@ export const Popover = forwardRef<any, PropsWithChildren<PopoverProps>>((props, 
         anchorEl={anchorE1}
         // @ts-ignore
         open={open}
-        disableRestoreFocus={triggerType === 'hover'}
+        disableRestoreFocus={triggerType === "hover"}
         {...restProps}
         sx={{
           ...(restProps?.sx || {}),
-          pointerEvents: triggerType === 'hover' ? 'none' : undefined,
+          pointerEvents: triggerType === "hover" ? "none" : undefined,
         }}
       >
         {content ?? children}
@@ -68,10 +63,10 @@ export const Popover = forwardRef<any, PropsWithChildren<PopoverProps>>((props, 
 });
 
 Popover.defaultProps = {
-  anchorOrigin: { vertical: 'top', horizontal: 'center' },
-  transformOrigin: { vertical: 'bottom', horizontal: 'center' },
+  anchorOrigin: { vertical: "top", horizontal: "center" },
+  transformOrigin: { vertical: "bottom", horizontal: "center" },
   PaperProps: { sx: { p: 0.5, borderRadius: 2 } },
-  triggerType: 'hover',
+  triggerType: "hover",
 };
 
-Popover.displayName = 'iimm.Mui.Popover';
+Popover.displayName = "iimm.Mui.Popover";
